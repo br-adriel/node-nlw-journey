15061268d1f99d44c22777792dcf9b996d518b5c
Adriel <adriel.fsantos@outlook.com>
2024-07-09 00:20:36 -0300

    feat: Add route for trip confirmation

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from 'fastify-type-provider-zod';
 import { setupDayjs } from './lib/dayjs';
 import { createTrip } from './routes/trip/create';
+import { confirmTrip } from './routes/trip/confirm';
 
 setupDayjs();
 const app = fastify();
@@ -13,6 +14,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('SERVER RUNNING');
