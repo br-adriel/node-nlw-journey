75e5d4f0864b7fa35f6ae1960372d51dc4c8a959
Adriel <adriel.fsantos@outlook.com>
2024-07-08 23:48:43 -0300

    feat: Update trip creation to save owner

--- src/routes/trip/create.ts
+++ src/routes/trip/create.ts
@@ -1,8 +1,10 @@
+import dayjs from 'dayjs';
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import nodemailer from 'nodemailer';
 import { z } from 'zod';
+import { getMailClient } from '../../lib/nodemailer';
 import { prisma } from '../../lib/prisma';
-import dayjs from 'dayjs';
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -13,20 +15,51 @@ export async function createTrip(app: FastifyInstance) {
           destination: z.string().min(4),
           starts_at: z.coerce.date(),
           ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
         }),
       },
     },
     async (req, res) => {
-      const { starts_at, ends_at } = req.body;
+      const { starts_at, ends_at, owner_name, owner_email, destination } =
+        req.body;
 
-      if (dayjs(starts_at).isBefore(new Date()))
+      if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error('Invalid trip start date');
+      }
 
-      if (dayjs(ends_at).isBefore(starts_at))
+      if (dayjs(ends_at).isBefore(starts_at)) {
         throw new Error('Invalid trip end date');
+      }
 
       const trip = await prisma.trip.create({
-        data: req.body,
+        data: {
+          destination,
+          ends_at,
+          starts_at,
+          participants: {
+            create: {
+              email: owner_email,
+              name: owner_name,
+              is_owner: true,
+              is_confirmed: true,
+            },
+          },
+        },
+      });
+
+      const mail = await getMailClient();
+      const message = await mail.sendMail({
+        from: {
+          name: 'Equipe plann.er',
+          address: 'equipe@plann.er',
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: 'Testando o envio de email',
+        html: '<p>Teste do envio de email</p>',
       });
       return {
         tripId: trip.id,
