5fe71d7129019b2e46dfd8e0b8f2338fe837ee5b
Adriel <adriel.fsantos@outlook.com>
2024-07-09 11:28:24 -0300

    feat: Add endpoint for participant confirmation

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
+import './lib/dayjs';
+import { confirmParticipant } from './routes/participant/confirm';
 import { confirmTrip } from './routes/trip/confirm';
 import { createTrip } from './routes/trip/create';
-import './lib/dayjs';
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('SERVER RUNNING');
