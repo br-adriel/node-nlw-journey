6e7fceff5cfc90eef91a0d15888fc53446f99f45
Adriel <adriel.fsantos@outlook.com>
2024-07-08 23:22:06 -0300

    feat: Add endpoint for creating trips

---
+++ src/routes/trip/create.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { prisma } from '../../lib/prisma';
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (req, res) => {
+      const { body } = req;
+      const trip = await prisma.trip.create({
+        data: body,
+      });
+      return {
+        tripId: trip.id,
+      };
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import fastify from 'fastify';
+import { createTrip } from './routes/trip/create';
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('SERVER RUNNING');
